6c2868148e248aa726e4f18d993025a564e70ebc
Xiao Hu <hu440@purdue.edu>
2019-03-14 13:59:54 -0400

    make it better

--- csgame/csgame/settings.py
+++ csgame/csgame/settings.py
@@ -122,7 +122,7 @@ if 'DATABASE_URL' in os.environ:
 
 import dj_database_url
 if in_heroku:
-    DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 else:
     DATABASES = {
         'default': {
