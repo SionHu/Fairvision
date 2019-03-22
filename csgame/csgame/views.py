2d4a8a2bc8d32c4ccbb6651d746e968286c5a9a2
Xiao Hu <hu440@purdue.edu>
2019-03-21 23:38:53 -0400

    add over page

--- csgame/csgame/views.py
+++ csgame/csgame/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render_to_response,redirect,render
 def profile(request):
     return render(request, 'profile.html')
 
+def over(request):
+    return render(request, 'over.html')
+
 def phase02(request):
     return render(request, 'phase02.html')
 
