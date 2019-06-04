58a7537a2fe19cf6aab5f03469691462d9afb6e9
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2019-06-04 13:41:38 -0400

    Change way to ask for answer in Phase 01a to make rephrasing easier. Fix 404.

--- users/views/rephrasing.py
+++ users/views/rephrasing.py
@@ -9,9 +9,11 @@ PREPOSITIONS = ("aboard", "about", "above", "across", "after", "against",
     "until", "up", "upon", "versus", "via", "with", "within", "without")
 TWO_WORD_PREPOSITIONS = ("left of", "right of", "close to", "back to",
     "counter to", "far from", "other than")
-PLURAL_NAME_OF_OBJECT = 'planes'
+IMG_PROMPT='cats,'
 
-def rephrase(q, a, img=PLURAL_NAME_OF_OBJECT+','):
+def rephraseList(qaDict):
+    return [rephrase(*i) for i in qaDict.items()]
+def rephrase(q, a):
     _, is_are, *q_words = q.split()
     q_words[-1] = q_words[-1][:-1]
     prepLoc = getFirstIndex(q_words)
@@ -20,25 +22,37 @@ def rephrase(q, a, img=PLURAL_NAME_OF_OBJECT+','):
             prep = q_words[prepLoc]
             fragment = ' '.join(q_words[prepLoc+1:])
             if q_words[1] in ('color', 'pattern'):
-                return ' '.join(['With most', img, fragment, 'should be', a]) + "."
+                return ' '.join(['With most', IMG_PROMPT, fragment, is_are, a]) + "."
         fragment = ' '.join(q_words)
-        return ' '.join(['With most', img, a, 'should be', fragment]) + "."
+        return ' '.join(['With most', IMG_PROMPT, a, is_are, fragment]) + "."
     else:
         if q_words[0] == 'the' and 'is' in q_words:
             fragment = ' '.join(q_words[2:])
-            return ' '.join(['With most', img, fragment, a]) + "."
+            return ' '.join(['With most', IMG_PROMPT, fragment, a]) + "."
         fragment = ' '.join(q_words)
-        return ' '.join(['With most', img, fragment, 'should be', a]) + "."
+        return ' '.join(['With most', IMG_PROMPT, fragment, is_are, a]) + "."
 def getFirstIndex(input, matches=PREPOSITIONS):
     for index, word in enumerate(input):
         if word in matches:
             return index
     return None
-
-def rephrase_old(q, a, img=PLURAL_NAME_OF_OBJECT+','):
+def rephrase_old(q, a):
     _, is_are, *q_words = q.split()
     fragment = ' '.join(q_words)[:-1]
-    if q_words[0] in PREPOSITIONS or ' '.join(q_words[:2]) in TWO_WORD_PREPOSITIONS:
-        return ' '.join(['With most', img, a, 'should be', fragment]) + "."
+    if a.endswith(fragment):
+        a = a[:len(a)-len(fragment)-1]
+    if getFirstIndex(['as', 'has', 'is'], a.split()):
+        return ' '.join(['With most', IMG_PROMPT, a]) + "."
+    elif q_words[0] in PREPOSITIONS or ' '.join(q_words[:2]) in TWO_WORD_PREPOSITIONS:
+        return ' '.join(['With most', IMG_PROMPT, a, is_are, fragment]) + "."
     else:
-        return ' '.join(['With most', img, fragment, 'should be', a]) + "."
+        return ' '.join(['With most', IMG_PROMPT, fragment, is_are, a]) + "."
+
+if __name__ == '__main__':
+    IMG_PROMPT = input('Objects: ') + ','
+    print()
+    while True:
+        q = input('Q: ')
+        a = input('A: ')
+        print(rephrase_old(q, a))
+        print()
