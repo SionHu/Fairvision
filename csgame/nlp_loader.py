3c9d8f50b785a73167e906f63fd6032b16baff4f
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2019-06-11 15:49:51 -0400

    Upgrade to medium

--- csgame/nlp_loader.py
+++ csgame/nlp_loader.py
@@ -1,7 +1,7 @@
-import en_core_web_sm
+from en_core_web_md import load
 import spacy
 
-nlp = en_core_web_sm.load()  # Single load in memory till killed
+nlp = load()  # Single load in memory till killed
 
 if not isinstance(nlp, spacy.lang.en.English):
     raise TypeError("Model given is not of type {}.".format("spacy.lang.en.English"))
