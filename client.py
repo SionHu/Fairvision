87a9ba9620f6fa60a3a6bfbad8e2ecc04b9d4746
SionHu <xiaohu@Xiaos-MacBook-Pro.local>
2019-06-12 14:14:50 -0400

    new server from nlp_host

--- client.py
+++ client.py
@@ -6,31 +6,20 @@ Script will connect to the server that we setup to reduce the nlp time stuff.
 import socket
 import comm as comm
 import csv
+import requests
 
 
 def send__receive_data(new_Qs, old_Qs):
-    """
-    Method to send the data to the server and get the result from it.
-    :param new_Qs: The new questions with IDs that need to be compared.
-    :param old_Qs: The old questions with IDs already in the database. Should be all dissimilar.
-    :return: new data returned by the server, which is in return data given by algorithm
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
-    sock.connect((comm.server_host, comm.server_port))
-    print("create a socket Successfully")
+    stuff_to_give = {"new_ques": new_Qs, "old_ques": old_Qs}
 
-    comm.send_data((new_Qs, old_Qs), sock)
-    print("Sent Data Successfully.")
-    result = comm.receive_data(sock)
-    print("Received Data Successfully")
-    sock.close()
+    print("before the server host")
+    res = requests.post('https://nlp-hosted.herokuapp.com/receive', json=stuff_to_give, verify=False)
+    print("AFTER the server host")
 
-    # do something with the result...
-    #print("result from server:")
-    #for row_rec in result[0]:
-    #    print(row_rec)
-    return result
+    if res.ok:
+       return res.json()
+    else:
+       print(res.reason)
 
 if __name__ == "__main__":
     old = []
