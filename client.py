38de73c9a7b2696afb31fbbe5dc62d6b86fe5c92
SionHu <xiaohu@Xiaos-MacBook-Pro.local>
2019-06-12 16:16:50 -0400

    resolve conflicts

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 Client code for server setup for nlp.
 Script will connect to the server that we setup to reduce the nlp time stuff.
@@ -62,47 +61,3 @@ if __name__ == "__main__":
         print("CHECK IF SERVER IS RUNNING !!")
 
     print("Run complete")
-=======
-"""
-Client code for server setup for nlp.
-Script will connect to the server that we setup to reduce the nlp time stuff.
-"""
-
-import socket
-import comm as comm
-import csv
-import requests
-
-
-def send__receive_data(new_Qs, old_Qs):
-    stuff_to_give = {"new_ques": new_Qs, "old_ques": old_Qs}
-
-    print("before the server host")
-    res = requests.post('https://nlp-hosted.herokuapp.com/receive', json=stuff_to_give, verify=False)
-    print("AFTER the server host")
-
-    if res.ok:
-       return res.json()
-    else:
-       print(res.reason)
-
-if __name__ == "__main__":
-    old = []
-    new = []
-    with open('Q & A - Haobo.csv') as test_att_csv:
-        csv_reader = csv.reader(test_att_csv, delimiter=',')
-        for row in csv_reader:
-            old.append(row)
-    with open('test_att.csv') as turdy:
-        csv_reader = csv.reader(turdy, delimiter=',')
-        for row in csv_reader:
-            new.append(row)
-    try:
-        print(send__receive_data(new, old))
-
-
-    except ConnectionRefusedError:
-        print("CHECK IF SERVER IS RUNNING !!")
-
-    print("Run complete")
->>>>>>> 2faf8d79d31808870b78a172b993d5b418471289
