feb3b2ee1e6e1f7cdddd200b83ba939afa643167
Haobo Wang <wang2940@purdue.edu>
2019-06-13 11:07:18 -0400

    remove unused files

--- client.py
+++ client.py
@@ -3,35 +3,8 @@ Client code for server setup for nlp.
 Script will connect to the server that we setup to reduce the nlp time stuff.
 """
 
-import socket
-import comm as comm
 import csv
 import requests
-
-
-def send__receive_data_old(new_Qs, old_Qs):
-    """
-    Method to send the data to the server and get the result from it.
-    :param new_Qs: The new questions with IDs that need to be compared.
-    :param old_Qs: The old questions with IDs already in the database. Should be all dissimilar.
-    :return: new data returned by the server, which is in return data given by algorithm
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
-    sock.connect((comm.server_host, comm.server_port))
-    print("create a socket Successfully")
-
-    comm.send_data((new_Qs, old_Qs), sock)
-    print("Sent Data Successfully.")
-    result = comm.receive_data(sock)
-    print("Received Data Successfully")
-    sock.close()
-
-    # do something with the result...
-    #print("result from server:")
-    #for row_rec in result[0]:
-    #    print(row_rec)
-    return result
 
 def send__receive_data(new_Qs, old_Qs):
     stuff_to_give = {"new_ques": new_Qs, "old_ques": old_Qs}
--- csgame/nlp_loader.py
+++
@@ -1,7 +0,0 @@
-import en_core_web_sm
-import spacy
-
-nlp = en_core_web_sm.load()  # Single load in memory till killed
-
-if not isinstance(nlp, spacy.lang.en.English):
-    raise TypeError("Model given is not of type {}.".format("spacy.lang.en.English"))
