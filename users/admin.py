e4e7aca3ac0b4a2c00ebd266d2d9a94b37d86995
SionHu <xiaohu@pal-nat186-79-222.itap.purdue.edu>
2019-06-26 14:20:25 -0400

    fix the typo of csv file generated from question modle

--- users/admin.py
+++ users/admin.py
@@ -232,7 +232,7 @@ class AnswerInline(admin.TabularInline):
     extra = 0
 
 class QuestionAdmin(admin.ModelAdmin):
-    actions = [export_csv('phase3-questions.csv', ['text','isFinal','skipCount','workerId'])]
+    actions = [export_csv('phase3-questions.csv', ['text','isFinal','skipCount','workerID'])]
     inlines = [
         AnswerInline,
     ]
