07c210f66df9e561ac69f45b771f6c01d0c134ce
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2019-09-05 13:46:01 -0400

    Add support for numbers in answer merging

--- p2nlp.py
+++ p2nlp.py
@@ -7,32 +7,30 @@ Data Output: dict that 1 answer corresponds to 1 question
 # impoer django settings module to make this script work separately
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csgame.settings")
+os.environ.setdefault("SPACY_WARNING_IGNORE", "W008")
 import django
 django.setup()
 from users.models import Question, Answer, Attribute
 from django.db.models import F, Q
-from phase2_reducer import AnswerReducer
-from users.views.rephrasing import rephrase_new as rephrase
+import phase2_reducer
+from users.views.rephrasing import rephrase_old as rephrase
 
 '''
 script function for phase02
 '''
 def phase02():
+    import importlib
+    importlib.reload(phase2_reducer)
     # Get all the queries for answer and question from database
-    answer_list = Answer.objects.values_list('text', 'question_id')
-    # print("answer list: ", answer_list)
+    return phase2_reducer.AnswerReducer().reduce_within_groups(Answer.objects.values_list('text', 'question_id'))
 
-
-    result_dict = AnswerReducer().reduce_within_groups(answer_list)
-
+if __name__ == "__main__":
     Answer.objects.update(isFinal=False, count=0)
-    for q, (a, count) in result_dict.items():
+    for q, (a, count) in phase02().items():
         print("a is: ", a)
         Answer.objects.filter(text=a, question_id=q)[:1].update(isFinal=True, count=count)
     print("Update successfully!")
 
-if __name__ == "__main__":
-    phase02()
     # rephrase and import into attributes we have
     for answer in Answer.objects.filter(Q(isFinal=True) & ~Q(text='')):
         Attribute.objects.get_or_create(word=rephrase(answer.question.text, answer.text), answer=answer)
