10f01af14d444d046bfcd8f6e3b5e78a65dbd3ab
SionHu <hu440@purdue.edu>
2020-04-26 15:14:24 -0400

    asd

--- paper_fig/plotter02.py
+++ paper_fig/plotter02.py
@@ -5,46 +5,47 @@ import csv, random
 from sklearn import metrics
 from sklearn.metrics import roc_auc_score
 
-EXP_NUM = 1
-VERSION = 4  # 3 or 4
-TYPE = "cluster"
-KIND = "B"
-
 # read the csv file
-# file  = open('./exp0{}_data.csv'.format(str(EXP_NUM)), "rt", encoding="utf-8")
-file  = open('./ce_{}.csv'.format(TYPE), "rt", encoding="utf-8")
+
+file  = open('./ce_random.csv', "rt", encoding="utf-8")
 data = csv.reader(file, delimiter=",")
 matrix = [row for row in data] # read the data from the files
 # cut til only precision and recall score of each row
 matrix = np.delete(matrix, (0), axis=0)
 matrix = np.delete(matrix, np.s_[0:5], axis=1)
 
-matrix_curr = matrix[:, [2,VERSION]]
-print(matrix_curr, "\n")
-
-# ind = np.argsort(matrix_curr, axis=0)
-# print(ind)
-
-# arr = [1, 3, 5, 2, 4, 6]
-# print(np.argsort(arr))
-
+matrix_ran = matrix[:, [2,3,4]]
+print("Rando, results:\n", matrix_ran, "\n")
 
 file.close()
 
-def cal_pr():
+
+file2  = open('./ce_cluster.csv', "rt", encoding="utf-8")
+data2 = csv.reader(file2, delimiter=",")
+matrix2 = [row for row in data2] # read the data from the files
+# cut til only precision and recall score of each row
+matrix2 = np.delete(matrix2, (0), axis=0)
+matrix2 = np.delete(matrix2, np.s_[0:5], axis=1)
+
+matrix_clu = matrix2[:, [2,3,4]]
+print("Cluster results:\n", matrix_clu, "\n")
+
+file2.close()
+
+def cal_pr(matrix):
     # relevent_all = matrix[len(matrix)-1][1]
-    relevent_all = len(matrix_curr)
+    relevent_all = len(matrix)
     precision, recall = [], []
     retrived = 0
     count = 1
-    for row in matrix_curr:
+    for row in matrix:
         pre_score, rec_score = 1, 0
         if row[0] == "TRUE": retrived += 1
         if count != 0: pre_score = retrived / (count)
         precision.append(pre_score)
         recall.append(int(row[1])/int(relevent_all))
 
-        print('precision:', pre_score, '\trecall:', retrived/relevent_all)
+        # print('precision:', pre_score, '\trecall:', retrived/relevent_all)
         count += 1
 
     # for the interpolation plot
@@ -69,8 +70,8 @@ def output_fig(pre_int, rec_int, precision, recall):
     # ax.plot(recall, precision, '--y') # this is the original curve
     # ax.step(recall, precision_int, '--r') # this is the interpolation curve
     # ax.plot(rec_int, pre_int, "-b", label="Experimnet {} outputs".format(EXP_NUM)) # final curve
-    ax.plot(rec_int, pre_int, "-b", label="{}_{}".format(TYPE, KIND)) # final curve
-    plt.fill_between(rec_int, pre_int, alpha=0.2, color='b')
+    ax.plot(rec_int, pre_int, "-b", label=TYPE) # final curve
+    plt.fill_between(rec_int, pre_int, alpha=0.2, color='r')
     # ax.plot(rec_int, precision2, "-r", label="Experimnet 2 outputs")
 
     ax.axis('equal')
@@ -90,4 +91,4 @@ def closest(lst, K):
      return idx
 
 if __name__ == '__main__':
-    cal_pr()
+    cal_pr(matrix_ran)
