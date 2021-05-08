cde797ba57d817ddcd1ee2984df659599078e6c7
Gore Kao <gkao@purdue.edu>
2021-05-08 11:34:31 -0400

    add output result csv and refactor output file paths

--- data_helper/fetch.py
+++ data_helper/fetch.py
@@ -3,13 +3,18 @@ import shutil
 import os
 from tqdm import tqdm
 
+######## parameters ########
 category = "dog"
-path = "/home/fairvision/sites/fairvision.net/datasets/flickr30k_images/results.csv"  # change path if needed
-source = "/home/fairvision/sites/fairvision.net/datasets/flickr30k_images/flickr30k_images/"  # change if needed
-dest = category  # create folder first
-pattern = "^{}s? | {}s? | {}s?$".format(category, category, category)  # change regex if needed
+in_csv = "/home/fairvision/sites/fairvision.net/datasets/flickr30k_images/results.csv"
+img_src = "/home/fairvision/sites/fairvision.net/datasets/flickr30k_images/flickr30k_images/"
+dest = category
+out_images = dest + "/" + dest
+out_csv = dest + "/" + dest + ".csv"
+pattern = "^{}s? | {}s? | {}s?$".format(category, category, category)
+############################
 
-df = pd.read_csv(path, sep="|", header=0, names=['image_name', 'comment_number', 'comment'])
+
+df = pd.read_csv(in_csv, sep="|", header=0, names=['image_name', 'comment_number', 'comment'])
 df['comment'].str.lower()
 df.dropna(inplace=True)
 df['comment'] = df['comment'].str.replace(r'[^\w\s]+', '')
@@ -20,8 +25,10 @@ image_list = unique_images.tolist()
 
 print("number of images:", len(unique_images))
 
-if not os.path.exists(dest):
-    os.makedirs(dest)
+if not os.path.exists(out_images):
+    os.makedirs(out_images)
 
 for image in tqdm(image_list):
-	shutil.copy(source + image, dest)
+    shutil.copy(img_src + image, out_images)
+
+contain_values.to_csv(out_csv, index=False)
